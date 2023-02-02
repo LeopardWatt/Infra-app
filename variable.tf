0270372856c84cfb17bca3e6c0cdf97783e4eac7
LeopardWatt <leopard.watt@gmail.com>
2023-02-02 18:56:10 +0200

    fffff5

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" { 
-default = "AKIA36AADS5SO4CT2GX2"
+default = "AKIA36AADS5SO4CfhfhgT2GX2"
 }
 variable "aws_secret_key" {
-default = "FQD5D8MqRbdOrAaGvJrW8kAO8rZqDEyk94bLtPKT"
+default = "FQD5D8MqRbdOrAaGvJrW8kAO8rZqDEykfhfgh94bLtPKT"
 }
 provider "aws" {
 region = "eu-central-1"
