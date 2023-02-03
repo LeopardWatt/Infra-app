926ae9fd0b5b4d60c143357f21d2bff4e4c42e36
LeopardWatt <leopard.watt@gmail.com>
2023-02-03 16:58:27 +0200

    no secrets

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.44.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
 
+# Create a VPC
+resource "aws_vpc" "like_home" {
+  cidr_block = "192.168.0.0/25"
+}
+# Create a EC2
 resource "aws_instance" "My_Ubuntu_Terraform" {
   ami           = "ami-03e08697c325f02ab"
   instance_type = "t2.micro"
