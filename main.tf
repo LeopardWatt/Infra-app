4f5129fc44d3dc495da9e8cec3bbfa212f47dc51
LeopardWatt <leopard.watt@gmail.com>
2023-02-07 21:47:37 +0200

    with_vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Configure Terraform AWS Version
 terraform {
   required_providers {
     aws = {
@@ -13,13 +14,44 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "like_home" {
-  cidr_block = "192.168.0.0/25"
+resource "aws_vpc" "like_home_vpc" {
+  cidr_block = "192.168.112.0/24"
+  tags = {
+  Name = "like-home"
+  }
 }
-# Create a EC2
+
+# Create a Subnet
+resource "aws_subnet" "like_home_subnet" {
+  vpc_id            = "${aws_vpc.like_home_vpc.id}"
+  cidr_block        = "192.168.112.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+  Name = "like-home"
+  }
+}
+
+# Connect EC2 to Network
+resource "aws_network_interface" "like_home_interface" {
+  subnet_id   = "${aws_subnet.like_home_subnet.id}"
+  private_ips = ["192.168.112.100"]
+
+  tags = {
+  Name = "primary_network_interface"
+  }
+}
+
+
+# Create a EC2 instance
 resource "aws_instance" "My_Ubuntu_Terraform" {
   ami           = "ami-03e08697c325f02ab"
   instance_type = "t2.micro"
+
+  network_interface {
+  network_interface_id = "${aws_network_interface.like_home_interface.id}"
+  device_index         = 0
+  }
 
   tags = {
     Name    = "Terraform Ubuntu Server"
