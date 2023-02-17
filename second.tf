c1d34ce72781adbda2fe9386705bccfdac088c0f
LeopardWatt <leopard.watt@gmail.com>
2023-02-17 16:43:23 +0200

    nz3

--- second.tf
+++ second.tf
@@ -42,6 +42,16 @@ resource "aws_internet_gateway" "gw" {
  vpc_id = aws_vpc.main.id
  
  tags = {
-   Name = "Project VPC IG"
+   Name = "Project VPC Internet Gateway"
  }
+}
+
+# Create NAT_gateway
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.example.id
+
+  tags = {
+    Name = "Project VPC NAT Gateway"
+  }
 }
